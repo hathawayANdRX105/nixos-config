aff7303434e76a04d3785d373087932e5e763c02
hathawayANdRX105 <2635254302@qq.com>
2024-07-02 18:48:07 +0800

    add wechat

--- home/packages.nix
+++ home/packages.nix
@@ -1,5 +1,7 @@
-{ pkgs, ... }: {
+{ pkgs, inputs, ... }: {
+
   home.packages = (with pkgs; [
+    # screenshot
     slurp
     grim
     sway-contrib.grimshot
@@ -29,12 +31,24 @@
     nekoray # proxy
     # wechat-uos
     # zed-editor
-
-    hypridle
-    openssl
+    # inputs.nixos-cn.legacyPackages.${system}.netease-cloud-music
+    # inputs.nixos-cn.legacyPackages.${system}.wine-wechat
+    wechat-uos
 
     unzip
     wget
+
+    xdg-user-dirs # a tool to help manage user directories
   ]);
 
+  home.sessionVariables = {
+    XDG_DESKTOP_DIR = "$HOME/Desktop";
+    XDG_DOCUMENTS_DIR = "$HOME/Documents";
+    XDG_DOWNLOAD_DIR = "$HOME/Downloads";
+    XDG_MUSIC_DIR = "$HOME/Music";
+    XDG_PICTURES_DIR = "$HOME/Pictures";
+    XDG_PUBLICSHARE_DIR = "$HOME/Public";
+    XDG_TEMPLATES_DIR = "$HOME/Templates";
+    XDG_VIDEOS_DIR = "$HOME/Videos";
+  };
 }

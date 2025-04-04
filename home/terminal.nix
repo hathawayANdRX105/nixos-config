4cf3ac2dd7f3542a50ec13c193bb71b107e1ca8c
hathawayANdRX105 <2635254302@qq.com>
2025-04-05 00:38:21 +0800

    fix some keymap conflictions between alacritty&helix&zellij

--- home/terminal.nix
+++ home/terminal.nix
@@ -1,72 +1,39 @@
 { config, pkgs, ... }: {
-  # programs.kitty = {
-  #   enable = true;
-  #   themeFile = "shadotheme";
-  #   font = {
-  #     name = "JetBrainsMono Nerd Font";
-  #     size = 14;
-  #   };
-  # };
-
   programs.alacritty.enable = true;
   programs.alacritty.settings = {
     window.decorations = "None";
-    font.normal.family = "JetBrainsMono Nerd Font";
-    font.size = 14;
+    font.normal.family = "LXGW WenKai Mono";
+    font.size = 16;
     terminal.osc52 = "CopyPaste"; # clipboard copy and paste
     selection.save_to_clipboard = true;
     keyboard.bindings = [
+      # disable vi mode
       {
-        key = "c";
-        mods = "Control|Shift";
-        action = "Copy";
+        key = "Space";
+        mods = "Shift|Control";
+        mode = "~Search";
+        action = "none";
       }
       {
-        key = "v";
+        key = "Space";
+        mods = "Shift|Control";
+        mode = "~Search";
+        action = "none";
+      }
+      # disable search mode
+      {
+        key = "F";
         mods = "Control|Shift";
-        action = "Paste";
+        mode = "~Search";
+        action = "none";
       }
       {
-        key = "ArrowUp";
+        key = "B";
         mods = "Control|Shift";
-        action = "ScrollLineUp";
+        mode = "~Search";
+        action = "none";
       }
-      {
-        key = "ArrowDown";
-        mods = "Control|Shift";
-        action = "ScrollLineDown";
-      }
-    ];
 
-    # tokyo_night_enhanced
-    colors.cursor.cursor = "#787c99";
-    colors.primary = {
-      background = "#08080b";
-      foreground = "#787c99";
-    };
-    colors.selection = {
-      text = "CellForeground";
-      background = "#515c7e";
-    };
-    colors.normal = {
-      black = "#363b54";
-      red = "#f7768e";
-      green = "#41a6b5";
-      yellow = "#e0af68";
-      blue = "#7aa2f7";
-      magenta = "#bb9af7";
-      cyan = "#7dcfff";
-      white = "#787c99";
-    };
-    colors.bright = {
-      black = "#363b54";
-      red = "#f7768e";
-      green = "#41a6b5";
-      yellow = "#e0af68";
-      blue = "#7aa2f7";
-      magenta = "#bb9af7";
-      cyan = "#7dcfff";
-      white = "#787c99";
-    };
+    ];
   };
 }
